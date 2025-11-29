1a7a84d2becdb392e99755d2da4cd205702ee8f2
Maria Leonor Pinto Guedes <up202107691@edu.fe.up.pt>
2025-11-29 23:08:15 +0000

    New Class: Types of topologies

--- epidemic_algorithm_logic/src/main/java/supervisor/TopologyType.java
+++ epidemic_algorithm_logic/src/main/java/supervisor/TopologyType.java
@@ -1,4 +1,21 @@
-package java.supervisor;
+package supervisor;
 
-public enum TopologyType {
+public enum TopologyType
+{
+    FULL_MESH,
+    PARTIAL_MESH,
+    RING,
+    STAR;
+
+    public static TopologyType fromString(String input){
+
+        return switch(input.toLowerCase()){
+            case "full mesh" -> FULL_MESH;
+            case "partial mesh" -> PARTIAL_MESH;
+            case "ring" -> RING;
+            case "star" -> STAR;
+
+            default -> throw new IllegalStateException("Unexpected value: " + input.toLowerCase());
+        };
+    }
 }
