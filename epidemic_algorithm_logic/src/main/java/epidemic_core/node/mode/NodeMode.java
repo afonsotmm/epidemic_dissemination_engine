ff55e3d4f85e92f5445b716b433ac0f89e52ea5d
Maria Leonor Pinto Guedes <up202107691@edu.fe.up.pt>
2025-11-29 23:14:59 +0000

    New Class: Dissemination mode

--- epidemic_algorithm_logic/src/main/java/epidemic_core/node/mode/NodeMode.java
+++ epidemic_algorithm_logic/src/main/java/epidemic_core/node/mode/NodeMode.java
@@ -1,4 +1,19 @@
 package epidemic_core.node.mode;
 
-public enum NodeMode {
+public enum NodeMode
+{
+    PULL,
+    PUSH,
+    PUSHPULL;
+
+    public static NodeMode fromString(String input){
+
+        return switch(input.toLowerCase()){
+            case "pull" -> PULL;
+            case "push" -> PUSH;
+            case "pushpull" -> PUSHPULL;
+
+            default -> throw new IllegalStateException("Unexpected value: " + input.toLowerCase());
+        };
+    }
 }
