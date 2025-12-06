9cb3ac7fca3778b30e635dcd4e077b02d8c69a07
Maria Leonor Pinto Guedes <up202107691@edu.fe.up.pt>
2025-12-06 20:08:25 +0000

    New Message Type: End from UI

--- epidemic_algorithm_logic/src/main/java/epidemic_core/message/FromUI/EndMessage.java
+++ epidemic_algorithm_logic/src/main/java/epidemic_core/message/FromUI/EndMessage.java
@@ -1,4 +1,10 @@
 package epidemic_core.message.FromUI;
 
 public class EndMessage {
+    // Constructor
+    public EndMessage(){}
+
+    public static EndMessage decodeMessage(String message) {
+        return new EndMessage();
+    }
 }
