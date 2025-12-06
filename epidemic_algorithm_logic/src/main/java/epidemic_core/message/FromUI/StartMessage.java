58164951f93f7a2e525e244a9b3ee9ef265e68c5
Maria Leonor Pinto Guedes <up202107691@edu.fe.up.pt>
2025-12-06 20:09:01 +0000

    New Message Type: Start from UI

--- epidemic_algorithm_logic/src/main/java/epidemic_core/message/FromUI/StartMessage.java
+++ epidemic_algorithm_logic/src/main/java/epidemic_core/message/FromUI/StartMessage.java
@@ -1,4 +1,29 @@
 package epidemic_core.message.FromUI;
 
+import epidemic_core.message.FromNode.StatusMessage;
+
 public class StartMessage {
+    private Integer N;
+    private Integer sourceNodes;
+    private String topology;
+    private String mode;
+
+    // Constructor
+    public StartMessage(Integer N, Integer sourceNodes, String topology, String mode) {
+        this.N = N;
+        this.sourceNodes = sourceNodes;
+        this.topology = topology;
+        this.mode = mode;
+    }
+
+    public static StartMessage decodeMessage(String message) {
+        String[] parts = message.split(";");
+        return new StartMessage(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), parts[3], parts[4]);
+    }
+
+    // Getters
+    public Integer getN() { return N; }
+    public Integer getSourceNodes() { return sourceNodes; }
+    public String getTopology() { return topology; }
+    public String getMode() { return mode; }
 }
