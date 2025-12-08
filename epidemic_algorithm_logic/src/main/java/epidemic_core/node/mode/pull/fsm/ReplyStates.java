5d6acb12dbf8dcc40912a9ed94329bfb6a44f08b
afonsotmm <afonso.tomas.mateus@gmail.com>
2025-12-08 02:52:24 +0000

    New Pull architecture

--- epidemic_algorithm_logic/src/main/java/epidemic_core/node/mode/pull/fsm/ReplyStates.java
+++
@@ -1,6 +0,0 @@
-package epidemic_core.node.mode.pull.fsm;
-
-public enum ReplyStates {
-    IDLE,
-    REPLY
-}
---
+++ epidemic_algorithm_logic/src/main/java/epidemic_core/node/mode/pull/fsm/reply_fsm/states/ReplyStates.java
@@ -0,0 +1,6 @@
+package epidemic_core.node.mode.pull.fsm.reply_fsm;
+
+public enum ReplyStates {
+    IDLE,
+    REPLY
+}
